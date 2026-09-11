6aa73732200befb0232d657b2ad130927211b988
thrishi3025 <thrishithota@gmail.com>
2026-09-11 19:43:38 +0530

    Update 3_Data_Visualization.py

--- pages/3_Data_Visualization.py
+++ pages/3_Data_Visualization.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.title("IPL Data Visualization")
 
 # Load Dataset
-df = pd.read_csv(r"D:\ETA\IPL_Streamlit_Project\IPL_Matches_Data_2008_2026.csv")
+df = pd.read_csv("IPL_Matches_Data_2008_2026.csv")
 
 # Calculate Total Runs
 df["total_runs"] = (
